a5af58d0e149742f722bdc4fcabff88bdb67118b
DEERDEER <100037208+deeveer@users.noreply.github.com>
2024-10-02 01:21:59 +0800

    Update Service & Repo

--- src/main/java/com/ntoutakeout/backend/repository/StoreRepository.java
+++ src/main/java/com/ntoutakeout/backend/repository/StoreRepository.java
@@ -7,12 +7,17 @@ import java.util.ArrayList;
 
 @Repository
 public class StoreRepository {
+    Store a = new Store("AStore","St.AAA",4.4,30);
+    Store b = new Store("BStore","St.BBB",4.2,60);
+    Store c = new Store("CStore","St.CCC",4.6,70);
     private final ArrayList<Store> storeList;
-
+    // temp data
     public StoreRepository() {
         storeList = new ArrayList<>();
+        storeList.add(a);
+        storeList.add(b);
+        storeList.add(c);
     }
-
     public ArrayList<Store> getStoreList() {
         return storeList;
     }
--- src/main/java/com/ntoutakeout/backend/service/StoreService.java
+++ src/main/java/com/ntoutakeout/backend/service/StoreService.java
@@ -1,11 +1,27 @@
 package com.ntoutakeout.backend.service;
 
+import com.ntoutakeout.backend.entity.Store;
 import com.ntoutakeout.backend.repository.StoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Service
 public class StoreService {
     @Autowired
     private StoreRepository storeRepository;
+
+    public List<Store> getStoresSortedByRating() {
+        List<Store> storeList = storeRepository.getStoreList();
+        storeList.sort(Comparator.comparingDouble(Store::getRank).reversed()); // Sort descending by rating
+        return storeList;
+    }
+
+    public List<Store> getStoresSortedByTime() {
+        List<Store> storeList = storeRepository.getStoreList();
+        storeList.sort(Comparator.comparing(Store::getAddress)); // Sort by time in ascending order
+        return storeList;
+    }
 }
