90e623362b1a05cbf67669fb962d259c4f116475
hahacold <zerotrustbye@gmail.com>
2024-10-02 02:37:00 +0800

    Fix getStores Completed

--- src/main/java/com/ntoutakeout/backend/controller/StoreController.java
+++ src/main/java/com/ntoutakeout/backend/controller/StoreController.java
@@ -1,13 +1,12 @@
 package com.ntoutakeout.backend.controller;
 
-import com.ntoutakeout.backend.entity.Dishes;
+
 import com.ntoutakeout.backend.entity.Store;
 import com.ntoutakeout.backend.service.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,50 +18,18 @@ public class StoreController {
     private StoreService storeService;
 
 
-
-
-
     @GetMapping("/stores")
     public ResponseEntity<List<Store>> getStores(
-            @RequestParam(value = "sortBy", defaultValue = "rating") String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = "desc") String order) {
-
-        List<Store> storeList;
-
-        // 根據排序條件選擇排序方法
-        if ("time".equalsIgnoreCase(sortBy)) {
-            storeList = storeService.getStoresSortedByTime();
-        } else if ("rating".equalsIgnoreCase(sortBy)) {
-            storeList = storeService.getStoresSortedByRating();
-        } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-
-        // 根據 `order` 決定是否需要反轉排序順序
-        if ("asc".equalsIgnoreCase(order)) {
-            // 如果是升序，反轉結果列表
-            Collections.reverse(storeList);
-        } else if (!"desc".equalsIgnoreCase(order)) {
-            // 如果 `order` 不是 "asc" 或 "desc"，返回錯誤響應
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+            @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword,
+            @RequestParam(value = "sortBy", required = false) String sortBy,
+            @RequestParam(value = "sortDir", required = false) String sortDir) {
+            // defaultValue = "rating" ,defaultValue = "desc"
+        // 調用 StoreService 的方法來過濾和排序店家
+        List<Store> storeList = storeService.getStoresFilteredAndSorted(keyword, sortBy, sortDir);
 
         return storeList.isEmpty()
                 ? ResponseEntity.status(HttpStatus.NOT_FOUND).build()
                 : ResponseEntity.status(HttpStatus.OK).body(storeList);
     }
-
-//    @GetMapping("/stores/{id}/dishes")
-//    public ResponseEntity<ArrayList<Dishes>> getStoreMenu(@PathVariable("id") String storeId) {
-//        var store = storeMap.get(storeId);
-//        if (store == null) {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-//        } else {
-//            var menu = store.getMenu();
-//            return ResponseEntity.status(HttpStatus.OK).body(menu);
-//        }
-//    }
-
-
 
 }
