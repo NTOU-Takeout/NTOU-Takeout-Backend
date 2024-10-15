229d0264ee5d2c27d2625499b0aaee20067c5ebf
hahacold <zerotrustbye@gmail.com>
2024-10-16 01:13:50 +0800

    Update StoreController.java
    https://github.com/NTOU-Takeout/NTOU-Takeout-Backend/pull/11#issuecomment-2414188708

--- src/main/java/com/ntoutakeout/backend/controller/StoreController.java
+++ src/main/java/com/ntoutakeout/backend/controller/StoreController.java
@@ -18,69 +18,45 @@ public class StoreController {
     @Autowired
     private StoreService storeService;
 
-    // keyword: for searching keyword
-    // sortBy: input 'rank', 'distance', 'averagePrice', 'name'
-    // sortDir: input 'desc', 'asc'
-    @GetMapping("/getStores")
-    public ResponseEntity<List<Store>> getStores(
-            @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword,
-            @RequestParam(value = "sortBy", required = false, defaultValue = "name") String sortBy,
-            @RequestParam(value = "sortDir", required = false, defaultValue = "asc") String sortDir) {
 
-        List<Store> storeList = storeService.getStoresFilteredAndSorted(keyword, sortBy, sortDir);
-        return ResponseEntity.status(HttpStatus.OK).body(storeList);
-    }
 
     // keyword: for searching keyword
     // sortBy: input 'rank', 'distance', 'averagePrice', 'name'
     // sortDir: input 'desc', 'asc'
     @GetMapping("/getIdList")
-    public ResponseEntity<Map<String, List<String>>> getIdList(
+    public ResponseEntity<List<String>> getIdList(
             @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword,
             @RequestParam(value = "sortBy", required = false, defaultValue = "name") String sortBy,
             @RequestParam(value = "sortDir", required = false, defaultValue = "asc") String sortDir) {
 
         List<String> storeIdList = storeService.getStoresIdFilteredAndSorted(keyword, sortBy, sortDir);
 
-        Map<String, List<String>> response = new HashMap<>();
-        response.put("ids", storeIdList);
-
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        return ResponseEntity.status(HttpStatus.OK).body(storeIdList);
     }
 
     @PostMapping("/getStoresByIdList")
-    public ResponseEntity<Map<String, List<Store>>> getStoresByIdList(@RequestBody Map<String, List<String>> request) {
-        List<String> storeIds = request.get("ids");
+    public ResponseEntity<List<Store>> getStoresByIdList(@RequestBody List<String> storeIds) {
 
         List<Store> stores = storeService.getStoreListByIds(storeIds);
-        Map<String, List<Store>> response = new HashMap<>();
-        response.put("stores", stores);
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(stores);
     }
 
 
     @GetMapping("/{storeId}/review")
-    public ResponseEntity<Map<String, List<Review>>> getReviewByStoreId(@PathVariable String storeId) {
-
+    public ResponseEntity<List<Review>> getReviewByStoreId(@PathVariable String storeId) {
 
         List<Review> reviewList = storeService.getReviewById(storeId);
 
-        Map<String, List<Review>> response = new HashMap<>();
-        response.put("reviews", reviewList);
-
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        return ResponseEntity.status(HttpStatus.OK).body(reviewList);
     }
 
     @GetMapping("/{storeId}/menu")
-    public ResponseEntity<Map<String, List<Menu>>> getMenuByStoreId(@PathVariable String storeId) {
+    public ResponseEntity<List<Menu>> getMenuByStoreId(@PathVariable String storeId) {
 
         List<Menu> dishList = storeService.getMenuById(storeId);
 
-        Map<String, List<Menu>> response = new HashMap<>();
-        response.put("menu", dishList);
-
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        return ResponseEntity.status(HttpStatus.OK).body(dishList);
     }
 
 
