0a0cb86a2527310091283f8b6c6114ab2d55b9bf
owen0806 <owen080614@gmail.com>
2024-10-20 16:41:34 +0800

    Add getDIshesByIds api

--- src/main/java/com/ntoutakeout/backend/repository/DishRepository.java
+++ src/main/java/com/ntoutakeout/backend/repository/DishRepository.java
@@ -4,5 +4,4 @@ import com.ntoutakeout.backend.entity.Dish;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface DishRepository extends MongoRepository<Dish, String> {
-    Dish findById(int id);
 }
