40925eb9b2712f5ad82d5dc6d3724ed5d35e2eaf
owen0806 <owen080614@gmail.com>
2024-10-30 23:31:35 +0800

    Add User collection and User id

--- src/main/java/com/ntoutakeout/backend/entity/user/User.java
+++ src/main/java/com/ntoutakeout/backend/entity/user/User.java
@@ -2,10 +2,15 @@ package com.ntoutakeout.backend.entity.user;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
+@Document(collection = "user")
 public abstract class User {
+    @Id
+    private String id;
     private String name;
     private String email;
     private String password;
