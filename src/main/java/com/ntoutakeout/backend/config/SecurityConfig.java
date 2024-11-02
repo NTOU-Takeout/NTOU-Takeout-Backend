0af9248a5ca6ba7c598c37df22c4e8167527a6e0
Yuankai619 <Yuankai_@outlook.com>
2024-11-03 00:13:32 +0800

    Add requestMatcher

--- src/main/java/com/ntoutakeout/backend/config/SecurityConfig.java
+++ src/main/java/com/ntoutakeout/backend/config/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.ntoutakeout.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,6 +15,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http.build();
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(HttpMethod.GET,"/api/store/**","/api/menu/**").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/store/**","/api/review/**","/api/menu/**","/api/user/login","/api/user/signup","/api/user/getUserIdByEmail").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 }
