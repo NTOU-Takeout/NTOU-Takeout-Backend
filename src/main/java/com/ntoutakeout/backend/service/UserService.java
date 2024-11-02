93b3cd85936a6240b004e8f7bab055d1e7f31cd4
Yuankai619 <Yuankai_@outlook.com>
2024-11-03 03:44:04 +0800

    Add Bcrypt encoder

--- src/main/java/com/ntoutakeout/backend/service/UserService.java
+++ src/main/java/com/ntoutakeout/backend/service/UserService.java
@@ -3,6 +3,7 @@ package com.ntoutakeout.backend.service;
 import com.ntoutakeout.backend.entity.user.User;
 import com.ntoutakeout.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,11 +11,13 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final static BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
     public User createUser(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
