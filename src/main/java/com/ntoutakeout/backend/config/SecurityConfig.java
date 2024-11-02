d9313e6bbc5c4c70d30ba423adf1daef07277dad
Yuankai619 <Yuankai_@outlook.com>
2024-11-03 00:33:44 +0800

    Add CustomUserDetailService and AuthenticationProvider

--- src/main/java/com/ntoutakeout/backend/config/SecurityConfig.java
+++ src/main/java/com/ntoutakeout/backend/config/SecurityConfig.java
@@ -1,27 +1,45 @@
 package com.ntoutakeout.backend.config;
 
+import com.ntoutakeout.backend.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers(HttpMethod.GET,"/api/store/**","/api/menu/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/menu/**").permitAll()
                         .requestMatchers(HttpMethod.POST,"/api/store/**","/api/review/**","/api/menu/**","/api/user/login","/api/user/signup","/api/user/getUserIdByEmail").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        authProvider.setUserDetailsService(customUserDetailsService);
+        return authProvider;
+    }
+
 }
