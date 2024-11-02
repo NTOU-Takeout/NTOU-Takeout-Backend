22fced5aa12eb20523b6253c5d3cf94b5b6a8ee1
Yuankai619 <Yuankai_@outlook.com>
2024-11-03 03:30:49 +0800

    Implement basic authentication
    - Change `User ` class to not abstract
    - Adjust  `UserController`
    - Add `CustomUserDetail `
    - Add `findByEmail` in UserRepository
    - Implement basic `CustomUserDetailsService `

--- src/main/java/com/ntoutakeout/backend/controller/UserController.java
+++ src/main/java/com/ntoutakeout/backend/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ntoutakeout.backend.controller;
 
 import com.ntoutakeout.backend.entity.Store;
 import com.ntoutakeout.backend.entity.user.User;
+import com.ntoutakeout.backend.service.CustomUserDetailsService;
 import com.ntoutakeout.backend.service.StoreService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,95 +19,101 @@ import java.util.Map;
 @RequestMapping("/api/user")
 @Slf4j
 public class UserController {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
     @Autowired
-    private StoreService userService;
-
-    @GetMapping("/getUserIdByEmail")
-    public ResponseEntity<?> getUserIdByEmail(@RequestParam("email") String email) {
-        log.info("Fetch API: getUserIdByEmail Success");
-        try {
-            User user = userService.findByEmail(email);
-
-            if (user == null) {
-                log.error("User not found with email: " + email);
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
-            }
-
-            return ResponseEntity.ok(user);
-        } catch (Exception e) {
-            log.error("Error fetching user by email: " + email, e);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: " + e.getMessage());
-        }
+    public UserController(CustomUserDetailsService userService) {
+        this.customUserDetailsService = userService;
     }
+
+//    @GetMapping("/getUserIdByEmail")
+//    public ResponseEntity<?> getUserIdByEmail(@RequestParam("email") String email) {
+//        log.info("Fetch API: getUserIdByEmail Success");
+//        try {
+//            User user = userService.findByEmail(email);
+//
+//            if (user == null) {
+//                log.error("User not found with email: " + email);
+//                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+//            }
+//
+//            return ResponseEntity.ok(user);
+//        } catch (Exception e) {
+//            log.error("Error fetching user by email: " + email, e);
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: " + e.getMessage());
+//        }
+//    }
 
 
     @PostMapping("/signup")
-    public ResponseEntity<?> signUpUser(@RequestBody User user) {
+    public ResponseEntity<User> signUpUser(@RequestBody User user) {
         log.info("Fetch API: signup Success");
         try {
-            User newUser = userService.signUpUser(user);
+            User newUser = customUserDetailsService.createUser(user);
 
-            String token = userService.generateToken(newUser);
+//            String token = userService.generateToken(newUser);
 
-            return ResponseEntity.status(HttpStatus.CREATED)
-                    .header("Authorization", "Bearer " + token)
-                    .body(newUser);
+            return ResponseEntity.ok(user);
+//                    .header("Authorization", "Bearer " + token)
+//                    .body(newUser);
         } catch (Exception e) {
-            log.error("Signup failed for user: " + .getEmail(), e);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Signup failed: " + e.getMessage());
+            log.error("Signup failed for user: {}", user.getEmail(), e);
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Signup failed: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
     }
 
 
 
-
-    @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestBody User user) {
-        log.info("Fetch API: login Success");
-        try {
-            User loginUser = userService.loginUser(user);
-
-            String token = userService.generateToken(user);
-
-            return ResponseEntity.status(HttpStatus.CREATED)
-                    .header("Authorization", "Bearer " + token)
-                    .body(loginUser);
-        } catch (Exception e) {
-            log.error("Login failed for email: " + user.getEmail(), e);
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed: " + e.getMessage());
-        }
-    }
-
-    @PatchMapping("/update")
-    public ResponseEntity<User> updateUser(@RequestHeader("Authorization") String token,
-                                           @RequestBody Map<String, Object> userUpdates) {
-        log.info("Fetch API: update User");
-        try {
-            String jwtToken = token.replace("Bearer ", "");
-            Claims claims = validateToken(jwtToken);
-            if (claims == null) {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-            }
-
-            User updatedUser = userService.updateUser(userUpdates);
-
-            if (updatedUser == null) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-            }
-
-            return ResponseEntity.ok(updatedUser);
-
-        } catch (IllegalArgumentException e) {
-            log.error("Update failed due to invalid input: ", e);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        } catch (SecurityException e) {
-            log.error("Update failed due to authorization: ", e);
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        } catch (Exception e) {
-            log.error("Unexpected error updating user: ", e);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-    }
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<?> loginUser(@RequestBody User user) {
+//        log.info("Fetch API: login Success");
+//        try {
+//            User loginUser = userService.loginUser(user);
+//
+//            String token = userService.generateToken(user);
+//
+//            return ResponseEntity.status(HttpStatus.CREATED)
+//                    .header("Authorization", "Bearer " + token)
+//                    .body(loginUser);
+//        } catch (Exception e) {
+//            log.error("Login failed for email: " + user.getEmail(), e);
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed: " + e.getMessage());
+//        }
+//    }
+//
+//    @PatchMapping("/update")
+//    public ResponseEntity<User> updateUser(@RequestHeader("Authorization") String token,
+//                                           @RequestBody Map<String, Object> userUpdates) {
+//        log.info("Fetch API: update User");
+//        try {
+//            String jwtToken = token.replace("Bearer ", "");
+//            Claims claims = validateToken(jwtToken);
+//            if (claims == null) {
+//                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+//            }
+//
+//            User updatedUser = userService.updateUser(userUpdates);
+//
+//            if (updatedUser == null) {
+//                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+//            }
+//
+//            return ResponseEntity.ok(updatedUser);
+//
+//        } catch (IllegalArgumentException e) {
+//            log.error("Update failed due to invalid input: ", e);
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+//        } catch (SecurityException e) {
+//            log.error("Update failed due to authorization: ", e);
+//            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+//        } catch (Exception e) {
+//            log.error("Unexpected error updating user: ", e);
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+//        }
+//    }
 
 
 
--- src/main/java/com/ntoutakeout/backend/service/CustomUserDetailsService.java
+++ src/main/java/com/ntoutakeout/backend/service/CustomUserDetailsService.java
@@ -1,14 +1,42 @@
 package com.ntoutakeout.backend.service;
 
+import com.ntoutakeout.backend.entity.user.CustomUserDetail;
+import com.ntoutakeout.backend.entity.user.User;
+import com.ntoutakeout.backend.repository.UserRepository;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByEmail(username);
+        if(user == null) {
+            log.error("User not found");
+            throw new UsernameNotFoundException(username);
+        }
+        return new CustomUserDetail(user);
     }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
 }
