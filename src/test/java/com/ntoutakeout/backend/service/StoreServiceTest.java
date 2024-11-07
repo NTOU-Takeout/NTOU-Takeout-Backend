e35c1ea78b0b08e265ab7a1362c888c49c4aaef8
hahacold <zerotrustbye@gmail.com>
2024-11-08 00:26:57 +0800

    Upate StoreServiceTest(add more different cases)

--- src/test/java/com/ntoutakeout/backend/service/StoreServiceTest.java
+++ src/test/java/com/ntoutakeout/backend/service/StoreServiceTest.java
@@ -44,6 +44,81 @@ class StoreServiceTest {
         assertEquals(List.of("1", "2"), result);
         verify(storeRepository).findByNameContainingOnlyIdOrderByNameAsc(keyword);
     }
+    @Test
+    void testGetStoresIdFilteredAndSortedByNameDesc() {
+        // Arrange
+        String keyword = "store";
+        List<Store> mockStores = List.of(
+                new Store("2", "store2", "pic2", "0987654321", "address2", 4.0, List.of(), "menu2", 25.0, "desc2", null),
+                new Store("1", "store1", "pic1", "1234567890", "address1", 4.5, List.of(), "menu1", 20.0, "desc1", null)
+        );
+        when(storeRepository.findByNameContainingOnlyIdOrderByNameDesc(keyword)).thenReturn(mockStores);
+
+        // Act
+        List<String> result = storeService.getStoresIdFilteredAndSorted(keyword, "name", "desc");
+
+        // Assert
+        assertEquals(List.of("2", "1"), result);
+        verify(storeRepository).findByNameContainingOnlyIdOrderByNameDesc(keyword);
+    }
+
+    @Test
+    void testGetStoresIdFilteredAndSortedByRatingAsc() {
+        String keyword = "store";
+        List<Store> mockStores = List.of(
+                new Store("2", "store2", "pic2", "0987654321", "address2", 4.0, List.of(), "menu2", 25.0, "desc2", null),
+                new Store("1", "store1", "pic1", "1234567890", "address1", 4.5, List.of(), "menu1", 20.0, "desc1", null)
+        );
+        when(storeRepository.findByNameContainingOnlyIdOrderByRatingAsc(keyword)).thenReturn(mockStores);
+
+        List<String> result = storeService.getStoresIdFilteredAndSorted(keyword, "rating", "asc");
+
+        assertEquals(List.of("2", "1"), result);
+        verify(storeRepository).findByNameContainingOnlyIdOrderByRatingAsc(keyword);
+    }
+    @Test
+    void testGetStoresIdFilteredAndSortedByRatingDesc() {
+        String keyword = "store";
+        List<Store> mockStores = List.of(
+                new Store("1", "store1", "pic1", "1234567890", "address1", 4.5, List.of(), "menu1", 20.0, "desc1", null),
+                new Store("2", "store2", "pic2", "0987654321", "address2", 4.0, List.of(), "menu2", 25.0, "desc2", null)
+        );
+        when(storeRepository.findByNameContainingOnlyIdOrderByRatingDesc(keyword)).thenReturn(mockStores);
+
+        List<String> result = storeService.getStoresIdFilteredAndSorted(keyword, "rating", "desc");
+
+        assertEquals(List.of("1", "2"), result);
+        verify(storeRepository).findByNameContainingOnlyIdOrderByRatingDesc(keyword);
+    }
+
+    @Test
+    void testGetStoresIdFilteredAndSortedByAverageSpendAsc() {
+        String keyword = "store";
+        List<Store> mockStores = List.of(
+                new Store("1", "store1", "pic1", "1234567890", "address1", 4.5, List.of(), "menu1", 20.0, "desc1", null),
+                new Store("2", "store2", "pic2", "0987654321", "address2", 4.0, List.of(), "menu2", 25.0, "desc2", null)
+        );
+        when(storeRepository.findByNameContainingOnlyIdOrderByAverageSpendAsc(keyword)).thenReturn(mockStores);
+
+        List<String> result = storeService.getStoresIdFilteredAndSorted(keyword, "averageSpend", "asc");
+
+        assertEquals(List.of("1", "2"), result);
+        verify(storeRepository).findByNameContainingOnlyIdOrderByAverageSpendAsc(keyword);
+    }
+    @Test
+    void testGetStoresIdFilteredAndSortedByAverageSpendDesc() {
+        String keyword = "store";
+        List<Store> mockStores = List.of(
+                new Store("2", "store2", "pic2", "0987654321", "address2", 4.0, List.of(), "menu2", 25.0, "desc2", null),
+                new Store("1", "store1", "pic1", "1234567890", "address1", 4.5, List.of(), "menu1", 20.0, "desc1", null)
+        );
+        when(storeRepository.findByNameContainingOnlyIdOrderByAverageSpendDesc(keyword)).thenReturn(mockStores);
+
+        List<String> result = storeService.getStoresIdFilteredAndSorted(keyword, "averageSpend", "desc");
+
+        assertEquals(List.of("2", "1"), result);
+        verify(storeRepository).findByNameContainingOnlyIdOrderByAverageSpendDesc(keyword);
+    }
 
     @Test
     void testGetStoreByIds() {
