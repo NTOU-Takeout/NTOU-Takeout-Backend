80652ed05ab3a21b0381d1e0e4ebf21c0d3e7f8c
owen0806 <owen080614@gmail.com>
2024-11-13 11:53:30 +0800

    Update UserService

--- src/main/java/com/ntoutakeout/backend/service/UserService.java
+++ src/main/java/com/ntoutakeout/backend/service/UserService.java
@@ -2,9 +2,14 @@ package com.ntoutakeout.backend.service;
 
 import com.ntoutakeout.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
