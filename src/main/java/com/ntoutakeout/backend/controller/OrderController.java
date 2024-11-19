cdb4ed9a9945fff54dc00b7cd707438dbf2cc016
owen0806 <owen080614@gmail.com>
2024-11-20 02:38:27 +0800

    Update OrderController.java

--- src/main/java/com/ntoutakeout/backend/controller/OrderController.java
+++ src/main/java/com/ntoutakeout/backend/controller/OrderController.java
@@ -3,7 +3,7 @@ package com.ntoutakeout.backend.controller;
 import com.ntoutakeout.backend.entity.order.Order;
 import com.ntoutakeout.backend.entity.order.OrderedDish;
 import com.ntoutakeout.backend.repository.OrderRepository;
-import com.ntoutakeout.backend.service.JwtService;
+import com.ntoutakeout.backend.service.JWTService;
 import com.ntoutakeout.backend.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,46 +19,49 @@ import java.util.Optional;
 @Slf4j
 public class OrderController {
 
-    @Autowired
-    private OrderService orderService;
+    private final OrderService orderService;
+    private final JWTService jwtService;
 
     @Autowired
-    private JwtService jwtService;
-
-    @PostMapping("/create")
-    public ResponseEntity<String> createOrder(@RequestHeader("Authorization") String token,
-                                                   @RequestBody Map<String, Object> cartRequest) {
-
-        log.info("POST API: /cart/update/dishes - Create cart dishes request received");
-
-        if (!jwtService.validateToken(token)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        String customerId = jwtService.extractCustomerId(token);
-
-
-        String createdOrderId = orderService.createOrder(cartRequest, customerId);
-        return ResponseEntity.status(HttpStatus.OK).body(createdOrderId);
+    private OrderController(OrderService orderService, JWTService jwtService) {
+        this.orderService = orderService;
+        this.jwtService = jwtService;
     }
 
-    @PutMapping("/update/{OrderId}")
-    public ResponseEntity<String> updateOrder(
-            @PathVariable String OrderId,
-            @RequestHeader("Authorization") String token,
-            @RequestBody Map<String, Object> updateRequest) {
-
-        if (!jwtService.validateToken(token)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-
-        String orderId = orderService.updateOrder(updateRequest, OrderId);
-
-        if (orderId == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-
-        return ResponseEntity.ok(orderId);
-    }
+//    @PostMapping("/create")
+//    public ResponseEntity<String> createOrder(@RequestHeader("Authorization") String token,
+//                                                   @RequestBody Map<String, Object> cartRequest) {
+//
+//        log.info("POST API: /cart/update/dishes - Create cart dishes request received");
+//
+//        if (!jwtService.validateToken(token)) {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+//        }
+//        String customerId = jwtService.extractCustomerId(token);
+//
+//
+//        String createdOrderId = orderService.createOrder(cartRequest, customerId);
+//        return ResponseEntity.status(HttpStatus.OK).body(createdOrderId);
+//    }
+//
+//    @PutMapping("/update/{OrderId}")
+//    public ResponseEntity<String> updateOrder(
+//            @PathVariable String OrderId,
+//            @RequestHeader("Authorization") String token,
+//            @RequestBody Map<String, Object> updateRequest) {
+//
+//        if (!jwtService.validateToken(token)) {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+//        }
+//
+//        String orderId = orderService.updateOrder(updateRequest, OrderId);
+//
+//        if (orderId == null) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+//        }
+//
+//        return ResponseEntity.ok(orderId);
+//    }
 
 
 }
