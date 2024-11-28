f22df1573facffb133f8e4b2016f6fd3918bb0b0
owen0806 <owen080614@gmail.com>
2024-11-28 19:18:22 +0800

    Update OrderedDish: add equalsWithId method

--- src/main/java/com/ntoutakeout/backend/entity/order/OrderedDish.java
+++ src/main/java/com/ntoutakeout/backend/entity/order/OrderedDish.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Data
@@ -22,5 +23,13 @@ public class OrderedDish {
     public OrderedDish() {
         this.id = UUID.randomUUID().toString();
         this.chosenAttributes = new ArrayList<>();
+    }
+
+    public boolean equalsWithoutId(OrderedDish o) {
+        return Objects.equals(dishId, o.dishId) &&
+                Objects.equals(dishName, o.dishName)
+                && Objects.equals(price, o.price)
+                && Objects.equals(note, o.note)
+                && Objects.equals(chosenAttributes, o.chosenAttributes);
     }
 }
