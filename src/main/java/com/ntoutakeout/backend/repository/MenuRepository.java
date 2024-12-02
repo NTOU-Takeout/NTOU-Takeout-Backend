85ab279bd22f0a24ef8346136e540ad2f63eab56
owen0806 <owen080614@gmail.com>
2024-12-02 17:38:52 +0800

    Rename project name

--- src/main/java/com/ntoutakeout/backend/repository/MenuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ntoutakeout.backend.repository;
-
-import com.ntoutakeout.backend.entity.Menu;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MenuRepository extends MongoRepository<Menu, String> {
-}
