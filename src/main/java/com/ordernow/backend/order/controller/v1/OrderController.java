f90c4fe308d3a0640a5dd541be52362b72dfea3e
owen0806 <owen080614@gmail.com>
2024-12-06 00:19:34 +0800

    Update order controller and service: update order status api

--- src/main/java/com/ordernow/backend/order/controller/v1/OrderController.java
+++ src/main/java/com/ordernow/backend/order/controller/v1/OrderController.java
@@ -1,6 +1,7 @@
 package com.ordernow.backend.order.controller.v1;
 
 import com.ordernow.backend.common.dto.ApiResponse;
+import com.ordernow.backend.order.model.entity.OrderedStatus;
 import com.ordernow.backend.order.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,11 +28,47 @@ public class OrderController {
     @PreAuthorize("hasAnyRole('CUSTOMER', 'MERCHANT')")
     public ResponseEntity<ApiResponse<Void>> cancelOrder(
             @PathVariable("orderId") String orderId)
-            throws NoSuchElementException {
+            throws NoSuchElementException, IllegalStateException {
 
         orderService.cancelOrder(orderId);
         ApiResponse<Void> apiResponse = ApiResponse.success(null);
-        log.info("Customer cancel dish successfully");
+        log.info("Cancel order successfully");
+        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+    }
+
+    @PatchMapping("/{orderId}/accept")
+    @PreAuthorize("hasAnyRole('MERCHANT')")
+    public ResponseEntity<ApiResponse<Void>> acceptOrder(
+            @PathVariable("orderId") String orderId)
+            throws NoSuchElementException, IllegalStateException {
+
+        orderService.updateStatus(orderId, OrderedStatus.PROCESSING);
+        ApiResponse<Void> apiResponse = ApiResponse.success(null);
+        log.info("Merchant accept order successfully");
+        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+    }
+
+    @PatchMapping("/{orderId}/complete")
+    @PreAuthorize("hasAnyRole('MERCHANT')")
+    public ResponseEntity<ApiResponse<Void>> completeOrder(
+            @PathVariable("orderId") String orderId)
+            throws NoSuchElementException, IllegalStateException {
+
+        orderService.updateStatus(orderId, OrderedStatus.COMPLETED);
+        ApiResponse<Void> apiResponse = ApiResponse.success(null);
+        log.info("Merchant complete order successfully");
+        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+    }
+
+    @PatchMapping("/{orderId}/pickup")
+    @PreAuthorize("hasAnyRole('CUSTOMER')")
+    public ResponseEntity<ApiResponse<Void>> pickUpOrder(
+            @PathVariable("orderId") String orderId)
+            throws NoSuchElementException, IllegalStateException {
+
+        orderService.updateStatus(orderId, OrderedStatus.PICKED_UP);
+        ApiResponse<Void> apiResponse = ApiResponse.success(null);
+        log.info("Customer picked up order successfully");
         return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
     }
 }
