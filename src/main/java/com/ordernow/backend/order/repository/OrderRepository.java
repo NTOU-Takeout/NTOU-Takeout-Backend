869abfe9c733a7be4157abb4fdff243879ce5bc1
owen0806 <owen080614@gmail.com>
2024-12-11 17:01:00 +0800

    Update OrderRepository.java

--- src/main/java/com/ordernow/backend/order/repository/OrderRepository.java
+++ src/main/java/com/ordernow/backend/order/repository/OrderRepository.java
@@ -2,7 +2,6 @@ package com.ordernow.backend.order.repository;
 
 import com.ordernow.backend.order.model.entity.Order;
 import com.ordernow.backend.order.model.entity.OrderedStatus;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
