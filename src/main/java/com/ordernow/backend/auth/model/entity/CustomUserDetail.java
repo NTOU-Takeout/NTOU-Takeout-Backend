ed6c33254e31b1fc8f4f99d56f3653273fc76e2f
owen0806 <owen080614@gmail.com>
2024-12-15 15:10:37 +0800

    Delete identical method in CustomerUserDetail

--- src/main/java/com/ordernow/backend/auth/model/entity/CustomUserDetail.java
+++ src/main/java/com/ordernow/backend/auth/model/entity/CustomUserDetail.java
@@ -34,24 +34,4 @@ public class CustomUserDetail implements UserDetails {
     public String getUsername() {
         return user.getEmail();
     }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
