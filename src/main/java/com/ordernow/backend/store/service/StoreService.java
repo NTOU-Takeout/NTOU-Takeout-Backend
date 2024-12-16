a1a05dd3084862d4aa4d44c276d477ad4075327e
owen0806 <owen080614@gmail.com>
2024-12-16 23:52:47 +0800

    Add set-store-info, change-business-status api

--- src/main/java/com/ordernow/backend/store/service/StoreService.java
+++ src/main/java/com/ordernow/backend/store/service/StoreService.java
@@ -1,6 +1,7 @@
 package com.ordernow.backend.store.service;
 
 import com.ordernow.backend.menu.service.MenuService;
+import com.ordernow.backend.store.model.dto.StoreUpdateRequest;
 import com.ordernow.backend.store.model.entity.Store;
 import com.ordernow.backend.store.repository.StoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -56,10 +57,31 @@ public class StoreService {
                 .collect(Collectors.toList());
     }
 
-    public String createAndSaveStore() {
+    public String createAndSaveStore(String phoneNumber) {
         Store store = Store.createDefaultStore();
         String menuId = menuService.createAndSaveMenu();
         store.setMenuId(menuId);
+        store.setPhoneNumber(phoneNumber);
         return storeRepository.save(store).getId();
+    }
+
+    public void updateStore(String storeId, StoreUpdateRequest request)
+            throws NoSuchElementException {
+
+        Store currentStore = getStoreById(storeId);
+        currentStore.setName(request.getName());
+        currentStore.setPicture(request.getPicture());
+        currentStore.setAddress(request.getAddress());
+        currentStore.setDescription(request.getDescription());
+        currentStore.setBusinessHours(request.getBusinessHours());
+        storeRepository.save(currentStore);
+    }
+
+    public boolean changeBusinessStatus(String storeId)
+            throws NoSuchElementException {
+        Store store = getStoreById(storeId);
+        store.setIsBusiness(!store.getIsBusiness());
+        storeRepository.save(store);
+        return store.getIsBusiness();
     }
 }
