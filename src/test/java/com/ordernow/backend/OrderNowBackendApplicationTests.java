551a40b12486737d4d9c506896b420b3cf9f5702
owen0806 <owen080614@gmail.com>
2024-12-21 22:22:34 +0800

    Update

--- src/test/java/com/ordernow/backend/OrderNowBackendApplicationTests.java
+++ src/test/java/com/ordernow/backend/OrderNowBackendApplicationTests.java
@@ -2,8 +2,6 @@ package com.ordernow.backend;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @SpringBootTest
 class OrderNowBackendApplicationTests {
 }
